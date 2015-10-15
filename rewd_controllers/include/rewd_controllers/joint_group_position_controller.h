542fcf3f36132d28c3b62c6ff0162104995b3ee1
Clint Liddick <clint.liddick@gmail.com>
2015-10-15 10:06:26 -0400

    "it builds" initial commit of a group position->effort controller that will include robot dynamics and kinematics in control loop

---
+++ rewd_controllers/include/rewd_controllers/joint_group_position_controller.h
@@ -0,0 +1,163 @@
+// TODO license/acknowlegement
+
+#ifndef REWD_CONTROLLERS__JOINT_GROUP_POSITION_CONTROLLER_H
+#define REWD_CONTROLLERS__JOINT_GROUP_POSITION_CONTROLLER_H
+
+/**
+   @class rewd_controllers::JointGroupPositionController
+   @brief Joint Group Position Controller
+
+   This class controls positon using a pid loop.
+
+   @section ROS ROS interface
+
+   @param type Must be "rewd_controllers::JointGroupPositionController"
+   @param joint Name of the joint to control.
+   @param pid Contains the gains for the PID loop around position.  See: control_toolbox::Pid
+
+   Subscribes to:
+
+   - @b command (std_msgs::Float64) : The joint position to achieve.
+
+   Publishes:
+
+   - @b state (control_msgs::JointControllerState) : // TODO array of?
+     Current state of the controller, including pid error and gains.
+
+*/
+
+#include <boost/scoped_ptr.hpp>
+#include <boost/thread/condition.hpp>
+#include <control_msgs/JointControllerState.h>
+#include <control_toolbox/pid.h>
+#include <controller_interface/controller.h>
+#include <hardware_interface/joint_command_interface.h>
+#include <kdl_extension/KdlTreeId.h>
+#include <realtime_tools/realtime_buffer.h>
+#include <realtime_tools/realtime_publisher.h>
+#include <ros/node_handle.h>
+#include <sensor_msgs/JointState.h>
+#include <urdf/model.h>
+
+namespace rewd_controllers
+{
+
+class JointGroupPositionController: public controller_interface::Controller<hardware_interface::EffortJointInterface>
+{
+public:
+
+  /**
+   * \brief Store position and velocity command in struct to allow easier realtime buffer usage
+   */
+  struct Commands
+  {
+    double position_; // Last commanded position
+    double velocity_; // Last commanded velocity
+    bool has_velocity_; // false if no velocity command has been specified
+  };
+
+  JointGroupPositionController();
+  ~JointGroupPositionController();
+
+  /** \brief The init function is called to initialize the controller from a
+   * non-realtime thread with a pointer to the hardware interface, itself,
+   * instead of a pointer to a RobotHW.
+   *
+   * \param robot The specific hardware interface used by this controller.
+   *
+   * \param n A NodeHandle in the namespace from which the controller
+   * should read its configuration, and where it should set up its ROS
+   * interface.
+   *
+   * \returns True if initialization was successful and the controller
+   * is ready to be started.
+   */  
+  bool init(hardware_interface::EffortJointInterface *robot, ros::NodeHandle &n);
+
+  /*!
+   * \brief Give set position of the joint for next update: revolute (angle) and prismatic (position)
+   *
+   * \param command
+   */
+  void setCommands(std::vector<std::string> names, std::vector<double> pos_commands);
+
+  /*!
+   * \brief Give set position of the joint for next update: revolute (angle) and prismatic (position)
+   *        Also supports a target velocity
+   *
+   * \param pos_target - position setpoint
+   * \param vel_target - velocity setpoint
+   */
+  // void setCommand(double pos_target, double vel_target);
+
+  /** \brief This is called from within the realtime thread just before the
+   * first call to \ref update
+   *
+   * \param time The current time
+   */
+  void starting(const ros::Time& time);
+  
+  /*!
+   * \brief Issues commands to the joint. Should be called at regular intervals
+   */
+  void update(const ros::Time& time, const ros::Duration& period);
+
+  /**
+   * \brief Get the PID parameters
+   */
+  // void getGains(double &p, double &i, double &d, double &i_max, double &i_min);
+
+  /**
+   * \brief Print debug info to console
+   */
+  // void printDebug();
+
+  /**
+   * \brief Get the PID parameters
+   */
+  // void setGains(const double &p, const double &i, const double &d, const double &i_max, const double &i_min);
+
+  /**
+   * \brief Get the name of the joint this controller uses
+   */
+  // std::vector<std::string> getJointNames();
+
+  /**
+   * \brief Get the current position of the joint
+   * \return current position
+   */
+  // double getPosition();
+
+  std::map<std::string, hardware_interface::JointHandle> joints_;
+  std::map<std::string, boost::shared_ptr<const urdf::Joint> > joint_urdfs_;
+  realtime_tools::RealtimeBuffer<std::map<std::string,Commands> > commands_;
+  std::map<std::string,Commands> command_structs_; // pre-allocated memory that is re-used to set the realtime buffer
+
+private:
+  int loop_count_;
+  control_toolbox::Pid pid_controller_;       /**< Internal PID controller. */
+  kdl_extension::KdlTreeId kdl_tree_id;
+
+  boost::scoped_ptr<
+    realtime_tools::RealtimePublisher<
+      control_msgs::JointControllerState> > controller_state_publisher_ ;
+
+  ros::Subscriber sub_command_;
+
+  /**
+   * \brief Callback from /command subscriber for setpoint
+   */
+  void setCommandCB(const sensor_msgs::JointStateConstPtr& msg);
+
+  /**
+   * \brief Check that the command is within the hard limits of the joint. Checks for joint
+   *        type first. Sets command to limit if out of bounds.
+   * \param command - the input to test
+   */
+  // void enforceJointLimits(double &command);
+
+};
+
+} // namespace
+
+#endif
---
+++ rewd_controllers/src/rewd_controllers/joint_group_position_controller.cpp
@@ -0,0 +1,242 @@
+// TODO license/acknowlegement
+
+#include <rewd_controllers/joint_group_position_controller.h>
+#include <angles/angles.h>
+#include <kdl/tree.hpp>
+#include <kdl_extension/KdlChainIdRne.h>
+#include <pluginlib/class_list_macros.h>
+
+using namespace rewd_controllers;
+
+JointGroupPositionController::JointGroupPositionController()
+  : loop_count_(0)
+{}
+
+JointGroupPositionController::~JointGroupPositionController()
+{
+  sub_command_.shutdown();
+}
+
+bool JointGroupPositionController::init(hardware_interface::EffortJointInterface *robot, ros::NodeHandle &n)
+{
+  // Get joint name from parameter server
+  std::vector<std::string> joint_names;
+  if (!n.getParam("joints", joint_names)) 
+  {
+    ROS_ERROR("No joints given (namespace: %s)", n.getNamespace().c_str());
+    return false;
+  }
+
+  // Load PID Controller using gains set on parameter server
+  if (!pid_controller_.init(ros::NodeHandle(n, "pid")))
+    return false;
+
+  // Start realtime state publisher
+  controller_state_publisher_.reset(
+    new realtime_tools::RealtimePublisher<control_msgs::JointControllerState>(n, "state", 1));
+
+  // Start command subscriber
+  sub_command_ = n.subscribe<sensor_msgs::JointState>("command", 1, &JointGroupPositionController::setCommandCB, this);
+
+  // Get URDF
+  urdf::Model urdf;
+  if (!urdf.initParam("robot_description"))
+  {
+    ROS_ERROR("Failed to parse urdf file");
+    return false;
+  }
+
+  // Get joint handles and URDFS
+  for (std::vector<std::string>::iterator it = joint_names.begin(); it != joint_names.end(); ++it) {
+    joints_[*it] = robot->getHandle(*it);
+    joint_urdfs_[*it] = urdf.getJoint(*it);
+
+    if (!joint_urdfs_[*it])
+    {
+        ROS_ERROR("Could not find joint '%s' in urdf", it->c_str());
+        return false;
+    }
+  }
+
+  // TODO
+
+  // TODO load model from urdf
+  KDL::Tree kdl_tree;
+  kdl_tree_id.setTree(kdl_tree);
+  kdl_tree_id.loadFromParam("robot_description");
+  // JointDynamicsData jd;
+  // TODO set joints' actuatortype to FORCE
+
+
+  return true;
+}
+
+// void JointGroupPositionController::setGains(const double &p, const double &i, const double &d, const double &i_max, const double &i_min)
+// {
+//   pid_controller_.setGains(p,i,d,i_max,i_min);
+// }
+
+// void JointGroupPositionController::getGains(double &p, double &i, double &d, double &i_max, double &i_min)
+// {
+//   pid_controller_.getGains(p,i,d,i_max,i_min);
+// }
+
+// void JointGroupPositionController::printDebug()
+// {
+//   pid_controller_.printValues();
+//   // TODO print kdl_tree info?
+// }
+
+
+// Set the joint positions command
+  void JointGroupPositionController::setCommands(std::vector<std::string> names, std::vector<double> pos_commands)
+{
+  for (int i = 0; i < pos_commands.size(); ++i) {
+    command_structs_[names[i]].position_ = pos_commands[i];
+    command_structs_[names[i]].has_velocity_ = false; // Flag to ignore the velocity command since our setCommand method did not include it
+  }
+
+  // the writeFromNonRT can be used in RT, if you have the guarantee that
+  //  * no non-rt thread is calling the same function (we're not subscribing to ros callbacks)
+  //  * there is only one single rt thread
+  commands_.writeFromNonRT(command_structs_); // TODO
+}
+
+// // Set the joint position command with a velocity command as well
+// void JointPositionController::setCommand(double pos_command, double vel_command)
+// {
+//   command_struct_.position_ = pos_command;
+//   command_struct_.velocity_ = vel_command;
+//   command_struct_.has_velocity_ = true;
+
+//   command_.writeFromNonRT(command_struct_);
+// }
+
+void JointGroupPositionController::starting(const ros::Time& time)
+{
+  // TODO joints
+  // double pos_command = joint_.getPosition();
+
+  // // Make sure joint is within limits if applicable
+  // enforceJointLimits(pos_command);
+
+  // command_struct_.position_ = pos_command;
+  // command_struct_.has_velocity_ = false;
+
+  // command_.initRT(command_struct_);
+
+  // pid_controller_.reset();
+}
+
+void JointGroupPositionController::update(const ros::Time& time, const ros::Duration& period)
+{
+  // TODO joints
+  
+  command_structs_ = *(commands_.readFromRT()); // TODO map of pointers?
+  // double command_position = command_struct_.position_;
+  // double command_velocity = command_struct_.velocity_;
+  // bool has_velocity_ =  command_struct_.has_velocity_;
+
+  // double error, vel_error;
+  // double commanded_effort;
+
+  // double current_position = joint_.getPosition();
+
+  // // Make sure joint is within limits if applicable
+  // enforceJointLimits(command_position);
+
+  // // Compute position error
+  // if (joint_urdf_->type == urdf::Joint::REVOLUTE)
+  // {
+  //  angles::shortest_angular_distance_with_limits(
+  //     current_position,
+  //     command_position,
+  //     joint_urdf_->limits->lower,
+  //     joint_urdf_->limits->upper,
+  //     error);
+  // }
+  // else if (joint_urdf_->type == urdf::Joint::CONTINUOUS)
+  // {
+  //   error = angles::shortest_angular_distance(current_position, command_position);
+  // }
+  // else //prismatic
+  // {
+  //   error = command_position - current_position;
+  // }
+
+  // // Decide which of the two PID computeCommand() methods to call
+  // if (has_velocity_)
+  // {
+  //   // Compute velocity error if a non-zero velocity command was given
+  //   vel_error = command_velocity - joint_.getVelocity();
+
+  //   // Set the PID error and compute the PID command with nonuniform
+  //   // time step size. This also allows the user to pass in a precomputed derivative error.
+  //   commanded_effort = pid_controller_.computeCommand(error, vel_error, period);
+  // }
+  // else
+  // {
+  //   // Set the PID error and compute the PID command with nonuniform
+  //   // time step size.
+  //   commanded_effort = pid_controller_.computeCommand(error, period);
+  // }
+
+  // // TODO set pos, vel, accl for every joint on kdl model
+  // KDL::Tree& tree = kdl_tree_id.getTree(); // TODO is this for one joint or all? how to get all joint pos
+  // // TODO joint.getForce to get inverse dynamics
+  // // TODO 1. log combined, ID, and PID effort
+  // // TODO 2. add ID to PID effort
+  // // TODO double check herb model inertia w/ data sheet
+  // // TODO check torques needed by PID to keep arm still (inertia)
+
+  // // joint_.setCommand(commanded_effort); // TODO joints
+
+  // // publish state
+  // if (loop_count_ % 10 == 0)
+  // {
+  //   if(controller_state_publisher_ && controller_state_publisher_->trylock())
+  //   {
+  //     controller_state_publisher_->msg_.header.stamp = time;
+  //     controller_state_publisher_->msg_.set_point = command_position;
+  //     controller_state_publisher_->msg_.process_value = current_position;
+  //     controller_state_publisher_->msg_.process_value_dot = joint_.getVelocity();
+  //     controller_state_publisher_->msg_.error = error;
+  //     controller_state_publisher_->msg_.time_step = period.toSec();
+  //     controller_state_publisher_->msg_.command = commanded_effort;
+
+  //     double dummy;
+  //     getGains(controller_state_publisher_->msg_.p,
+  //       controller_state_publisher_->msg_.i,
+  //       controller_state_publisher_->msg_.d,
+  //       controller_state_publisher_->msg_.i_clamp,
+  //       dummy);
+  //     controller_state_publisher_->unlockAndPublish();
+  //   }
+  // }
+  // loop_count_++;
+}
+
+void JointGroupPositionController::setCommandCB(const sensor_msgs::JointStateConstPtr& msg)
+{
+  setCommands(msg->name, msg->position);
+}
+
+// // Note: we may want to remove this function once issue https://github.com/ros/angles/issues/2 is resolved
+// void JointGroupPositionController::enforceJointLimits(double &command)
+// {
+//   // Check that this joint has applicable limits
+//   if (joint_urdf_->type == urdf::Joint::REVOLUTE || joint_urdf_->type == urdf::Joint::PRISMATIC)
+//   {
+//     if( command > joint_urdf_->limits->upper ) // above upper limnit
+//     {
+//       command = joint_urdf_->limits->upper;
+//     }
+//     else if( command < joint_urdf_->limits->lower ) // below lower limit
+//     {
+//       command = joint_urdf_->limits->lower;
+//     }
+//   }
+// }
+
+
+PLUGINLIB_EXPORT_CLASS( rewd_controllers::JointGroupPositionController, controller_interface::ControllerBase)
