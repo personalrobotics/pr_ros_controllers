4d1eadbf9b0f75a024478ae268ac0f00cf3acc9a
Clint Liddick <clint.liddick@gmail.com>
2016-03-22 15:15:04 -0400

    convert tare to more general trigger/triggerable
    tare logic can also be used for "resets" and
    other parameterless, triggerable actions

--- trigger_controller/src/TriggerController.cpp
+++ trigger_controller/src/TriggerController.cpp
@@ -1,7 +1,7 @@
-#include <tare_controller/tare_controller.h>
+#include <trigger_controller/TriggerController.h>
 #include <pluginlib/class_list_macros.h>
 
-namespace tare_controller
+namespace trigger_controller
 {
 
 namespace
@@ -14,26 +14,26 @@ std::string getLeafNamespace(const ros::NodeHandle& nh)
 }
 } // namespace
 
-using pr_hardware_interfaces::TARE_IDLE;
-using pr_hardware_interfaces::TARE_REQUESTED;
-using pr_hardware_interfaces::TARE_PENDING;
+using pr_hardware_interfaces::TRIGGER_IDLE;
+using pr_hardware_interfaces::TRIGGER_REQUESTED;
+using pr_hardware_interfaces::TRIGGER_PENDING;
 
 
-bool TareController::init(TareInterface* hw,
+bool TriggerController::init(TriggerableInterface* hw,
                           ros::NodeHandle& root_nh,
                           ros::NodeHandle& controller_nh)
 {
   controller_nh_ = controller_nh;
   controller_name_ = getLeafNamespace(controller_nh_);
 
-  std::string tare_handle_name;
-  if (!controller_nh_.getParam("tare_handle_name", tare_handle_name)) {
-    ROS_ERROR_NAMED(controller_name_, "Failed loading resource name from 'tare_handle_name' parameter.");
+  std::string resource_name;
+  if (!controller_nh_.getParam("resource_name", resource_name)) {
+    ROS_ERROR_NAMED(controller_name_, "Failed loading resource name from 'resource_name' parameter.");
     return false;
   }
 
   try {
-    tare_handle_ = hw->getHandle(tare_handle_name);
+    triggerable_handle_ = hw->getHandle(resource_name);
   } catch(const std::logic_error& e) {
     ROS_ERROR_STREAM_NAMED(controller_name_, "Unable to initizize controller '" << controller_name_ << "'. " << e.what());
     return false;
@@ -50,8 +50,8 @@ bool TareController::init(TareInterface* hw,
                          "'" << "\n");
 
   // ROS API: Action interface
-  action_server_.reset(new ActionServer(controller_nh_, "tare",
-					boost::bind(&TareController::goalCB, this, _1),
+  action_server_.reset(new ActionServer(controller_nh_, "trigger",
+					boost::bind(&TriggerController::goalCB, this, _1),
 					false));
   action_server_->start();
   ROS_DEBUG_STREAM_NAMED(controller_name_, "ActionServer started");
@@ -59,40 +59,40 @@ bool TareController::init(TareInterface* hw,
   return true;
 }
 
-void TareController::update(const ros::Time& time, const ros::Duration& period)
+void TriggerController::update(const ros::Time& time, const ros::Duration& period)
 {
-  pr_hardware_interfaces::TareState tare_state = tare_state_.load();
+  pr_hardware_interfaces::TriggerState trigger_state = trigger_state_.load();
   RealtimeGoalHandlePtr rt_tmp_gh;
   rt_goal_.get(rt_tmp_gh);
 
-  if (tare_state == TARE_REQUESTED) {
-    if (tare_handle_.isTareComplete()) {
-      tare_handle_.tare();
-      tare_state_.store(TARE_PENDING);
+  if (trigger_state == TRIGGER_REQUESTED) {
+    if (triggerable_handle_.isTriggerComplete()) {
+      triggerable_handle_.trigger();
+      trigger_state_.store(TRIGGER_PENDING);
     }
     else {
       rt_tmp_gh->preallocated_result_->success = false;
-      rt_tmp_gh->preallocated_result_->message = "Tare is already in progress.";
+      rt_tmp_gh->preallocated_result_->message = "Request already triggered.";
       rt_tmp_gh->setAborted(rt_tmp_gh->preallocated_result_);
     }
   }
-  else if (tare_state == TARE_PENDING && tare_handle_.isTareComplete()) {
+  else if (trigger_state == TRIGGER_PENDING && triggerable_handle_.isTriggerComplete()) {
     rt_tmp_gh->preallocated_result_->success = true;
-    rt_tmp_gh->preallocated_result_->message = "Tare completed succcessfully.";
+    rt_tmp_gh->preallocated_result_->message = "Trigger completed succcessfully.";
     rt_tmp_gh->setSucceeded(rt_tmp_gh->preallocated_result_);
-    tare_state_.store(TARE_IDLE);
+    trigger_state_.store(TRIGGER_IDLE);
   }
 }
 
-void TareController::goalCB(GoalHandle gh)
+void TriggerController::goalCB(GoalHandle gh)
 {
-  ROS_DEBUG_STREAM_NAMED(controller_name_, "Recieved new tare request.");
-  pr_hardware_interfaces::TareState tare_state = tare_state_.load();
+  ROS_DEBUG_STREAM_NAMED(controller_name_, "Recieved new trigger request.");
+  pr_hardware_interfaces::TriggerState trigger_state = trigger_state_.load();
 
   // Precondition: Running controller
   if (!this->isRunning()) {
     ROS_ERROR_NAMED(controller_name_, "Can't accept new action goals. Controller is not running.");
-    pr_control_msgs::TareResult result;
+    pr_control_msgs::TriggerResult result;
     result.success = false;
     result.message = "Controller is not running.";
     gh.setRejected(result);
@@ -105,15 +105,15 @@ void TareController::goalCB(GoalHandle gh)
     // operations. We access the same shared_ptr (not a copy) from multiple threads here.
     // In C++11 the atomic functions of std::shared_ptr could be used instead.
     rt_goal_.set(rt_tmp_gh);
-    tare_state_.store(TARE_REQUESTED);
+    trigger_state_.store(TRIGGER_REQUESTED);
 
     service_update_timer_ = controller_nh_.createTimer(action_monitor_period_,
                                                        &RealtimeGoalHandle::runNonRealtime,
                                                        rt_tmp_gh);
-    ROS_DEBUG_STREAM_NAMED(controller_name_, "Tare action accepted.");
+    ROS_DEBUG_STREAM_NAMED(controller_name_, "Trigger action accepted.");
   }
 }
 
 } // namespace
 
-PLUGINLIB_EXPORT_CLASS(tare_controller::TareController, controller_interface::ControllerBase);
+PLUGINLIB_EXPORT_CLASS(trigger_controller::TriggerController, controller_interface::ControllerBase);
